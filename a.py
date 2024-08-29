666e3aeb5405bf0454044c8feded31fab0073987
yewengood <152235769+yewengood@users.noreply.github.com>
2024-08-29 09:16:14 +0800

    Update a.py

--- a.py
+++ a.py
@@ -156,52 +156,10 @@ with open("itvlist.txt", 'w', encoding='utf-8') as file:
                 file.write(channel + "\n")
                 channel_counters[channel_name] = 1
     channel_counters = {}
-    file.write('央视频道,#genre#\n')
-    for channel in channels:
-        channel_name,channel_url = channel.split(",")
-        if 'CCTV' in channel_name:
-            if channel_name in channel_counters:
-                if channel_counters[channel_name] >= result_counter:
-                    continue
-                else:
-                    file.write(channel + "\n")
-                    channel_counters[channel_name] += 1
-            else:
-                file.write(channel + "\n")
-                channel_counters[channel_name] = 1
-    channel_counters = {}  
-    file.write('数字频道,#genre#\n')
-    for channel in channels:
-        channel_name, channel_url = channel.split(",")
-        if '天元' in channel_name or '风云' in channel_name or '球' in channel_name or '影' in channel_name:
-            if channel_name in channel_counters:
-                if channel_counters[channel_name] >= result_counter:
-                    continue
-                else:
-                    file.write(f"{channel_name},{channel_url}\n")
-                    channel_counters[channel_name] += 1
-            else:
-                file.write(f"{channel_name},{channel_url}\n")
-                channel_counters[channel_name] = 1
-    channel_counters = {}
-    file.write('卫视频道,#genre#\n')
-    for channel in channels:
-        channel_name,channel_url = channel.split(",")
-        if '卫视' in channel_name:
-            if channel_name in channel_counters:
-                if channel_counters[channel_name] >= result_counter:
-                    continue
-                else:
-                    file.write(channel + "\n")
-                    channel_counters[channel_name] += 1
-            else:
-                file.write(channel + "\n")
-                channel_counters[channel_name] = 1
-    channel_counters = {}
     file.write('其他频道,#genre#\n')
     for channel in channels:
         channel_name,channel_url = channel.split(",")
-        if 'CCTV' not in channel_name and '卫视' not in channel_name and '测试' not in channel_name and '广东' not in channel_name and '广州' not in channel_name:
+        if '广东' not in channel_name and '广州' not in channel_name:
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
                     continue
@@ -229,7 +187,7 @@ with open("itvlist.m3u", 'a', encoding='utf-8') as file:
                 file.write(f"{channel_url}\n")
                 channel_counters[channel_name] = 1
                 channel_counters = {}
-    #file.write('#EXTM3U\n')
+    #file.write('广东频道,#genre#\n')
     for channel in channels:
         channel_name,channel_url = channel.split(",")
         if '广东' in channel_name or '广州' in channel_name:
