c59a37fd8e389462f7caac0397f50a098a2b4d3d
yewengood <152235769+yewengood@users.noreply.github.com>
2024-09-20 14:07:11 +0800

    Update tv.sh

--- speedtest/tv.sh
+++ speedtest/tv.sh
@@ -12,20 +12,20 @@ while IFS= read -r ip; do
     # 如果连接成功，且输出包含 "succeeded"，则将结果保存到输出文件中
     if [[ $output == *"succeeded"* ]]; then
         # 使用 awk 提取 IP 地址和端口号对应的字符串，并保存到输出文件中
-        echo "$output" | grep "succeeded" | awk -v ip="$ip" '{print ip}' >> "$only_good_ip"
+        echo "$output" | grep "succeeded" | awk -v ip="$ip" '{print ip}' >> "ip/${city}.onlygood.ip"
     fi
 done < "$ipfile"
 echo "===============检索完成================="
 
 # 检查文件是否存在
-if [ ! -f "$only_good_ip" ]; then
-    echo "错误：文件 $only_good_ip 不存在。"
+if [ ! -f "ip/${city}.onlygood.ip" ]; then
+    echo "错误：文件 ip/${city}.onlygood.ip 不存在。"
     exit 1
 fi
 
 
-lines=$(wc -l < "$only_good_ip")
-echo "【$only_good_ip】内 ip 共计 $lines 个"
+lines=$(wc -l < "ip/${city}.onlygood.ip")
+echo "【ip/${city}.onlygood.ip】内 ip 共计 $lines 个"
 
 i=0
 time=$(date +%Y%m%d%H%M%S) # 定义 time 变量
@@ -39,7 +39,7 @@ while IFS= read -r line; do
 
     echo "第 $i/$lines 个：$ip $a"
     echo "$ip $a" >> "speedtest_${city}_$time.log"
-done < "$only_good_ip"
+done < "ip/${city}.onlygood.ip"
 
 rm -f zubo.tmp
 awk '/M|k/{print $2"  "$1}' "speedtest_${city}_$time.log" | sort -n -r >"result_fofa_${city}.txt"
@@ -56,5 +56,6 @@ sed "s/ipipip/$ip1/g" "$program" > tmp1.txt
 sed "s/ipipip/$ip2/g" "$program" > tmp2.txt
 sed "s/ipipip/$ip3/g" "$program" > tmp3.txt
 cat tmp1.txt tmp2.txt tmp3.txt > "txt/fofa_${city}.txt"
+rm -rf tmp1.txt tmp2.txt tmp3.txt
 
 for a in result/*.txt; do echo "";echo "========================= $(basename "$a") ==================================="; cat $a; done
