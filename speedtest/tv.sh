3c7acbae5fbf09de2bc3ab78b10ca15c06b3f834
yewengood <152235769+yewengood@users.noreply.github.com>
2024-09-23 15:48:02 +0800

    Update tv.sh

--- speedtest/tv.sh
+++ speedtest/tv.sh
@@ -58,6 +58,7 @@ sed "s/ipipip/$ip2/g" "$program" > tmp2.txt
 sed "s/ipipip/$ip3/g" "$program" > tmp3.txt
 cat tmp1.txt tmp2.txt tmp3.txt > "txt/fofa_${city}.txt"
 rm -rf tmp1.txt tmp2.txt tmp3.txt
+rm -rf gdtv_fofa.txt
 #--------------------合并所有城市的txt文件为:   zubo_fofa.txt-----------------------------------------
 echo "📡  电信频道,#genre#" >>gdtv_fofa.txt
 cat txt/fofa_liantong1.txt >>gdtv_fofa.txt
