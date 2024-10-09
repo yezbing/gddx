ffcf41b531db33fb3d7bf90a4440bd8c9895ee75
yezbing <yezebingy@126.com>
2024-10-09 15:19:22 +0800

    Update dx.sh

--- speedtest/dx.sh
+++ speedtest/dx.sh
@@ -60,6 +60,6 @@ cat tmp1.txt tmp2.txt tmp3.txt > "txt/fofa_${city}.txt"
 rm -rf tmp1.txt tmp2.txt tmp3.txt
 rm -rf gddx_fofa.txt
 #--------------------合并所有城市的txt文件为:   zubo_fofa.txt-----------------------------------------
-echo "📡  电信频道,#genre#" >>gdtv_fofa.txt
+echo "📡  电信频道,#genre#" >>gddx_fofa.txt
 cat txt/fofa_dianxin.txt >>gddx_fofa.txt
 for a in result/*.txt; do echo "";echo "========================= $(basename "$a") ==================================="; cat $a; done
